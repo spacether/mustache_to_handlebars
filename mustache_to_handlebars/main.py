5d56fd6ef96d55e0ddeb0afde096d892a2006f4c
Justin Black <justin.a.black@gmail.com>
2021-10-09 16:08:15 -0700

    Removes constants

--- mustache_to_handlebars/main.py
+++ mustache_to_handlebars/main.py
@@ -5,11 +5,9 @@ import typing
 import re
 from enum import Enum
 
+TAG_OPEN = '{{'
+TAG_CLOSE = '}}'
 HANDLEBARS_EXTENSION = 'handlebars'
-HANDLEBARS_IF_CLOSE = '{{/if}}'
-HANDLEBARS_UNLESS_CLOSE = '{{/unless}}'
-HANDLEBARS_WITH_CLOSE = '{{/with}}'
-HANDLEBARS_EACH_CLOSE = '{{/each}}'
 HANDLEBARS_WHITESPACE_REMOVAL_CHAR = '~'
 HANDLEBARS_FIRST = '@first'
 HANDLEBARS_LAST = '@last'
@@ -33,22 +31,33 @@ class MustacheTagType(str, Enum):
     CLOSE = '/'
 
 class HandlebarsTagType(Enum):
-    IF = 0
-    EACH = 1
-    WITH = 2
+    # value is open prefix, close tag
+    IF = ('#if', '/if')
+    EACH = ('#each', '/each')
+    WITH = ('#with', '/with')
+    UNLESS = ('#unless', '/unless')
+    CLOSE = ('', '')
 
 def __get_handlebars_tag_type(
     tag: str,
+    mustache_tag_control_character: str,
     handlebars_if_tags: typing.Set[str],
     handlebars_each_tags: typing.Set[str],
     handlebars_with_tags: typing.Set[str]
 ) -> typing.Optional[HandlebarsTagType]:
-    if tag in handlebars_if_tags:
-        return HandlebarsTagType.IF
-    if tag in handlebars_each_tags:
-        return HandlebarsTagType.EACH
-    if tag in handlebars_with_tags:
-        return HandlebarsTagType.WITH
+    # mustache_tag_control_character: the #/^ control character
+    mustache_tag_type = MustacheTagType(mustache_tag_control_character)
+    if mustache_tag_type is MustacheTagType.IF_EACH_WITH:
+        if tag in handlebars_if_tags:
+            return HandlebarsTagType.IF
+        if tag in handlebars_each_tags:
+            return HandlebarsTagType.EACH
+        if tag in handlebars_with_tags:
+            return HandlebarsTagType.WITH
+    elif mustache_tag_type is MustacheTagType.UNLESS:
+        return HandlebarsTagType.UNLESS
+    elif mustache_tag_type is MustacheTagType.CLOSE:
+        return HandlebarsTagType.CLOSE
     return None
 
 def __mustache_to_handlebars_tag_element(mustache_tag_element: str) -> str:
@@ -136,7 +145,7 @@ def _convert_handlebars_to_mustache(
     closures = []
     for i in range(len(in_txt)):
         for tag_without_braces in tags:
-            tag_with_braces = '{{'+tag_without_braces+'}}'
+            tag_with_braces = TAG_OPEN + tag_without_braces + TAG_CLOSE
 
             end_index = i + len(tag_with_braces)
             if end_index > len(in_txt):
@@ -148,36 +157,27 @@ def _convert_handlebars_to_mustache(
                 tag = __mustache_to_handlebars_tag_element(tag)
                 handlebars_tag_type = __get_handlebars_tag_type(
                     tag,
+                    tag_without_braces[0],
                     handlebars_if_tags,
                     handlebars_each_tags,
                     handlebars_with_tags
                 )
-                if handlebars_tag_type is None:
-                    ambiguous_tags.add(tag)
-                mustache_tag_type = MustacheTagType(tag_without_braces[0])
 
-                if mustache_tag_type is MustacheTagType.IF_EACH_WITH:
-                    if handlebars_tag_type is HandlebarsTagType.IF:
-                        new_tag = '{{#if ' + tag + '}}'
-                        closures.append(HANDLEBARS_IF_CLOSE)
-                    elif handlebars_tag_type is HandlebarsTagType.EACH:
-                        new_tag = '{{#each ' + tag + '}}'
-                        closures.append(HANDLEBARS_EACH_CLOSE)
-                    elif handlebars_tag_type is HandlebarsTagType.WITH:
-                        new_tag = '{{#with ' + tag + '}}'
-                        closures.append(HANDLEBARS_WITH_CLOSE)
-                    else:
-                        new_tag = '{{#ifOrEachOrWith ' + tag + '}}'
-                        closures.append('{{/ifOrEachOrWith}}')
-                elif mustache_tag_type is MustacheTagType.UNLESS:
-                    new_tag = '{{#unless ' + tag + '}}'
-                    closures.append(HANDLEBARS_UNLESS_CLOSE)
-                elif mustache_tag_type is MustacheTagType.CLOSE:
-                    new_tag = closures.pop()
+                if (
+                    handlebars_tag_type in
+                    {HandlebarsTagType.IF, HandlebarsTagType.EACH, HandlebarsTagType.WITH, HandlebarsTagType.UNLESS}
+                ):
+                    new_tag = TAG_OPEN + handlebars_tag_type.value[0] + ' ' + tag + TAG_CLOSE
+                    closures.append(handlebars_tag_type.value[1])
+                elif handlebars_tag_type is None:
+                    ambiguous_tags.add(tag)
+                    new_tag = '{{#ifOrEachOrWith ' + tag + TAG_CLOSE
+                    closures.append('/ifOrEachOrWith')
+                elif handlebars_tag_type is HandlebarsTagType.CLOSE:
+                    new_tag = TAG_OPEN + closures.pop() + TAG_CLOSE
 
                 replacement_index_to_from_to_pair[i] = (tag_with_braces, new_tag)
                 break
-
 
     out_txt = str(in_txt)
     for i, (original_tag, new_tag) in reversed(replacement_index_to_from_to_pair.items()):
@@ -186,11 +186,10 @@ def _convert_handlebars_to_mustache(
     out_txt = _add_whitespace_handling(out_txt)
     return out_txt, ambiguous_tags
 
-def _create_files(in_path_to_out_path: dict) -> typing.List[str]:
+def _create_files(in_path_to_out_path: dict) -> typing.Tuple[typing.List[str], typing.Set[str]]:
     existing_out_folders = set()
     ambiguous_tags = set()
     input_files_used_to_make_output_files = []
-    skipped_files = 0
     for i, (in_path, out_path) in enumerate(in_path_to_out_path.items()):
         print('Reading file {} out of {}, path={}'.format(i+1, len(in_path_to_out_path), in_path))
         with open(in_path) as file:
@@ -199,7 +198,6 @@ def _create_files(in_path_to_out_path: dict) -> typing.List[str]:
         out_txt, file_ambiguous_tags = _convert_handlebars_to_mustache(in_txt, HANDLEBARS_IF_TAGS, HANDLEBARS_EACH_TAGS, HANDLEBARS_WITH_TAGS)
         if file_ambiguous_tags:
             ambiguous_tags.update(file_ambiguous_tags)
-            skipped_files += 1
             print('Skipped writing file {} because it has ambiguous tags'.format(out_path))
             continue
 
@@ -213,11 +211,7 @@ def _create_files(in_path_to_out_path: dict) -> typing.List[str]:
             file.write(out_txt)
         input_files_used_to_make_output_files.append(in_path)
         print('Wrote file {}'.format(out_path))
-
-    if ambiguous_tags:
-        print('len(ambiguous_tags)={}'.format(len(ambiguous_tags)))
-        print('skipped generating {} files'.format(skipped_files))
-        print('ambiguous_tags={}'.format(ambiguous_tags))
+    return input_files_used_to_make_output_files, ambiguous_tags
 
 def _clean_up_files(files_to_delete: typing.List[str]):
     if not files_to_delete:
@@ -227,6 +221,33 @@ def _clean_up_files(files_to_delete: typing.List[str]):
         print('Removing file {} out of {}, path={}'.format(i+1, len(files_to_delete), path))
         os.remove(path)
 
+def __handle_ambiguous_tags(ambiguous_tags: typing.Set[str], qty_skipped_files: int):
+    print('\nskipped generating {} files'.format(qty_skipped_files))
+    print('qty_ambiguous_tags={}'.format(len(ambiguous_tags)))
+    print('ambiguous_tags={}'.format(ambiguous_tags))
+
+    suspected_if_tags = []
+    suspected_each_tags = []
+    for tag in ambiguous_tags:
+        if (
+            tag.startswith('is') or
+            tag.startswith('has') or
+            tag.startswith('getHas') or
+            tag.startswith('use') or
+            tag.startswith('getIs')
+        ):
+            suspected_if_tags.append(tag)
+    ambiguous_tags = ambiguous_tags - set(suspected_if_tags)
+    for tag in ambiguous_tags:
+        if tag.endswith('s'):
+            suspected_each_tags.append(tag)
+    ambiguous_tags = ambiguous_tags - set(suspected_each_tags)
+    suspected_with_tags = list(ambiguous_tags)
+    print('here are some guesses at what your tags may be based only on tag names:\n')
+    print('if_tags=\"{}\"\n'.format(" ".join(suspected_if_tags)))
+    print('each_tags=\"{}\"\n'.format(" ".join(suspected_each_tags)))
+    print('with_tags=\"{}\"\n'.format(" ".join(suspected_with_tags)))
+
 
 def mustache_to_handlebars():
     in_dir, out_dir, recursive, delete_in_files = __get_args()
@@ -234,6 +255,13 @@ def mustache_to_handlebars():
         out_dir = in_dir
 
     in_path_to_out_path = _get_in_file_to_out_file_map(in_dir, out_dir, recursive)
-    input_files_used_to_make_output_files = _create_files(in_path_to_out_path)
+    input_files_used_to_make_output_files, ambiguous_tags = _create_files(in_path_to_out_path)
+
+    if ambiguous_tags:
+        __handle_ambiguous_tags(
+            ambiguous_tags,
+            len(in_path_to_out_path) - len(input_files_used_to_make_output_files)
+        )
+
     if delete_in_files:
         _clean_up_files(input_files_used_to_make_output_files)
